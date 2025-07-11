b0b040f2250b2d6d502c398db124f505ea8d0e6a
Tihan Pelser <tihanpelser@gmail.com>
2025-07-11 12:09:55 +0200

    Fixed indentation and missing paramaters

--- pythoncan.py
+++ pythoncan.py
@@ -1,3 +1,4 @@
+# Not yet tested!
 import argparse
 import time
 import uuid
@@ -12,12 +13,12 @@ import ssl
 #	time.sleep(1)
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-	    print("Connected with result code "+str(rc))
-	
-	    # Subscribing in on_connect() means that if we lose the connection and
-	    # reconnect then subscriptions will be renewed.
-	    client.subscribe(topic + "/+", qos=0)
+def on_connect(client, userdata, flags, rc, topic, *args, **kwargs):
+	print(f"Connected with result code {rc}")
+
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	client.subscribe(topic + "/+", qos=0)
 	
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
